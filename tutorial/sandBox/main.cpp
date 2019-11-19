d285c382dbf51bff2cec5ad4bcfeb1e03f17aa84
MaorRoz <andory@post.bgu.ac.il>
2019-11-19 18:03:05 +0200

    added wip MyViewer

--- tutorial/sandBox/main.cpp
+++ tutorial/sandBox/main.cpp
@@ -1,12 +1,13 @@
 
 #include "igl/opengl/glfw/renderer.h"
 #include "tutorial/sandBox/inputManager.h"
+#include "tutorial/sandBox/MyViewer.h"
 
 int main(int argc, char *argv[])
 {
   Display *disp = new Display(1000, 800, "Wellcome");
   Renderer renderer;
-  igl::opengl::glfw::Viewer viewer;
+  MyViewer viewer;
  // viewer.load_mesh_from_file("C:/Users/Owner/Desktop/animation/Ass1/AnimationAssignment1/tutorial/data/cube.obj");
  // viewer.load_mesh_from_file("C:/Users/Owner/Desktop/animation/Ass1/AnimationAssignment1/tutorial/data/bunny.off");
   viewer.load_mesh_from_file("C:/Users/User/Documents/dev/AnimationAssignment1/tutorial/data/sphere.obj");
