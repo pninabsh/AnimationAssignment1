cf08cff68dae1d85dcbefac1d09e5003b8462318
MaorRoz <andory@post.bgu.ac.il>
2019-11-22 16:07:12 +0200

    pnina save

--- tutorial/sandBox/mesh_simplifier.h
+++ tutorial/sandBox/mesh_simplifier.h
@@ -88,36 +88,3 @@ static void do_simplify(igl::opengl::glfw::Viewer *viewer) {
 	double rounded_up_five_percent_edges = std::ceil(0.05 * selectedSimplifyDataObject.E.rows());
 	simplify(rounded_up_five_percent_edges);
 }
-/*
-static std::vector<SimplifyDataObject> get_simplify_data_structures_list(std::vector<igl::opengl::ViewerData>  data_list) {
-	std::vector<SimplifyDataObject> simplifyDataObjectsList;
-
-	for (int i = 0; i < data_list.size(); i++) {
-		SimplifyDataObject simplifyDataObject;
-
-		igl::edge_flaps(data_list[i].F, simplifyDataObject.E, simplifyDataObject.EMAP, simplifyDataObject.EF, simplifyDataObject.EI);
-
-		simplifyDataObject.C.resize(simplifyDataObject.E.rows(), data_list[i].V.cols());
-
-		simplifyDataObject.Q.clear();
-
-		for (int e = 0; e < simplifyDataObject.E.rows(); e++)
-		{
-			Eigen::RowVectorXd p(0, 0);
-			simplifyDataObject.C.row(e) = p;
-
-			double cost = e;
-			simplifyDataObject.Q.insert(std::pair<double, int>(cost, e));
-		}
-
-
-		simplifyDataObjectsList.push_back(simplifyDataObject);
-	}
-
-	return simplifyDataObjectsList;
-}
-
-static void simplify(double number_of_edges) {
-	std::cout << number_of_edges << std::endl;
-}
-*/
