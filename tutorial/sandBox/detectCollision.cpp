135cca60ee61c9ef8782b925b28c345006aa283b
MaorRoz <andory@post.bgu.ac.il>
2020-01-04 23:27:48 +0200

    added collision detection

--- tutorial/sandBox/detectCollision.cpp
+++ tutorial/sandBox/detectCollision.cpp
@@ -1,41 +1,39 @@
 #include "detectCollision.h"
 
+Eigen::Vector4d calculate_point(Eigen::Vector3d point, igl::opengl::ViewerData collider) {
+	Eigen::Vector4d point4d = Eigen::Vector4d(point(0), point(1), point(2), 1);
+	return collider.MakeTrans().cast<double>() * point4d;
+}
+
 bool detect_x_collision(igl::opengl::ViewerData collider1, Eigen::AlignedBox<double, 3> bounding_box1,
 	igl::opengl::ViewerData collider2, Eigen::AlignedBox<double, 3> bounding_box2) {
-	Eigen::Vector3d oldX = bounding_box1.corner(bounding_box1.BottomLeft);
-	Eigen::Vector4d newX = Eigen::Vector4d(oldX(0), oldX(1), oldX(2), 1);
-	double X =  (collider1.MakeTrans().cast<double>() * newX)(0);
-
-	Eigen::Vector3d oldX2 = bounding_box2.corner(bounding_box2.BottomLeft);
-	Eigen::Vector4d newX2 = Eigen::Vector4d(oldX2(0), oldX2(1), oldX2(2), 1);
-	double X2 = (collider2.MakeTrans().cast<double>() * newX2)(0);
-	//double x2 = (collider1.MakeTrans() * bounding_box1.corner(bounding_box1.BottomRight))(0);
+	double x1 = calculate_point(bounding_box1.corner(bounding_box1.BottomLeft),collider1)(0);
+	double x2 = calculate_point(bounding_box1.corner(bounding_box1.BottomRight), collider1)(0);
 	
-	//double x3 = (collider2.MakeTrans() * bounding_box2.corner(bounding_box2.BottomLeft))(0);
-	//double x4 = (collider2.MakeTrans() * bounding_box2.corner(bounding_box2.BottomRight))(0);
+	double x3 = calculate_point(bounding_box2.corner(bounding_box2.BottomLeft), collider2)(0);
+	double x4 = calculate_point(bounding_box2.corner(bounding_box2.BottomLeft), collider2)(0);
 
-	//return (x3 >= x1 && x3 <= x2) || (x4 >= x1 && x4 <= x2);
-	return false;
+	return (x3 >= x1 && x3 <= x2) || (x4 >= x1 && x4 <= x2);
 }
 
 bool detect_y_collision(igl::opengl::ViewerData collider1, Eigen::AlignedBox<double, 3> bounding_box1,
 	igl::opengl::ViewerData collider2, Eigen::AlignedBox<double, 3> bounding_box2) {
-	double y1 = bounding_box1.corner(bounding_box1.BottomLeft)(1);
-	double y2 = bounding_box1.corner(bounding_box1.TopLeft)(1);
+	double y1 = calculate_point(bounding_box1.corner(bounding_box1.BottomLeft), collider1)(1);
+	double y2 = calculate_point(bounding_box1.corner(bounding_box1.TopLeft), collider1)(1);
 
-	double y3 = bounding_box1.corner(bounding_box2.BottomLeft)(1);
-	double y4 = bounding_box1.corner(bounding_box2.TopLeft)(1);
+	double y3 = calculate_point(bounding_box2.corner(bounding_box2.BottomLeft), collider2)(1);
+	double y4 = calculate_point(bounding_box2.corner(bounding_box2.TopLeft), collider2)(1);
 
 	return (y3 >= y1 && y3 <= y2) || (y4 >= y1 && y4 <= y2);
 }
 
 bool detect_z_collision(igl::opengl::ViewerData collider1, Eigen::AlignedBox<double, 3> bounding_box1,
 	igl::opengl::ViewerData collider2, Eigen::AlignedBox<double, 3> bounding_box2) {
-	double z1 = bounding_box1.corner(bounding_box1.BottomLeftFloor)(2);
-	double z2 = bounding_box1.corner(bounding_box1.BottomLeftCeil)(2);
+	double z1 = calculate_point(bounding_box1.corner(bounding_box1.BottomLeftFloor), collider1)(2);
+	double z2 = calculate_point(bounding_box1.corner(bounding_box1.BottomLeftCeil), collider1)(2);
 
-	double z3 = bounding_box1.corner(bounding_box2.BottomLeftFloor)(2);
-	double z4 = bounding_box1.corner(bounding_box2.BottomLeftCeil)(2);
+	double z3 = calculate_point(bounding_box2.corner(bounding_box2.BottomLeftFloor), collider2)(2);
+	double z4 = calculate_point(bounding_box2.corner(bounding_box2.BottomLeftCeil), collider2)(2);
 
 	return (z3 >= z1&& z3 <= z2) || (z4 >= z1 && z4 <= z2);
 }
